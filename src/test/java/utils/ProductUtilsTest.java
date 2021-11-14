93e9e996ebc932805a9cfb85201eceb8d04bc6e3
Shadan <shadan227@gmail.com>
2021-11-15 04:02:09 +0530

    Update .gitignore

--- src/test/java/utils/ProductUtilsTest.java
+++ src/test/java/utils/ProductUtilsTest.java
@@ -1,18 +1,9 @@
 package utils;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.UUID;
-
-import static org.junit.Assert.*;
-
 public class ProductUtilsTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void testGenerateIdReturnsUUID() {
